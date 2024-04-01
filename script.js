37ac34438c0187e070a5903712456c36d80397c8
Senhor_Ini <rafabs26126@gmail.com>
2024-04-01 15:13:23 -0300

    css

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Site de História - Ditadura Militar
 document.addEventListener("DOMContentLoaded", function() {
     var button = document.getElementById("drawer");
+    var button2 = document.getElementById("drawer2");
     var menu = document.querySelector(".menu");
   
     button.addEventListener("click", function() {
@@ -10,4 +11,11 @@ document.addEventListener("DOMContentLoaded", function() {
         menu.classList.add("open");
       }
     });
+    button2.addEventListener("click", function() {
+        if (menu.classList.contains("open")) {
+          menu.classList.remove("open");
+        } else {
+          menu.classList.add("open");
+        }
+      });
   });
